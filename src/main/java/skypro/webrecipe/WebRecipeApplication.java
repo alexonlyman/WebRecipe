63f160e8d487360e81c0f76739a77532d26decaf
Алекс <chu@mail.ru>
2023-01-24 21:15:34 +0300

    недостающие файлы

---
+++ src/main/java/skypro/webrecipe/WebRecipeApplication.java
@@ -0,0 +1,13 @@
+package skypro.webrecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRecipeApplication.class, args);
+    }
+
+}
