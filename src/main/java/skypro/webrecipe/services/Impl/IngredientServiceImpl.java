8085b6a9c75b5444ad77e387f21315dea59eaf72
Алекс <chu@mail.ru>
2023-02-06 17:23:07 +0300

    сохранение файлов

--- src/main/java/skypro/webrecipe/services/Impl/IngredientServiceImpl.java
+++ src/main/java/skypro/webrecipe/services/Impl/IngredientServiceImpl.java
@@ -73,11 +73,15 @@ public class IngredientServiceImpl implements IngredientService {
     private void readFromFile() {
         String json = fileSirvice.readFromFile();
         try {
-            ingredientMap = new ObjectMapper().readValue(json, new TypeReference<HashMap<Integer, Ingredient>>() {
+            try {
+                ingredientMap = new ObjectMapper().readValue(json, new TypeReference<HashMap<Integer, Ingredient>>() {
 
-            });
-        } catch (JsonProcessingException e) {
-            e.getStackTrace();
+                });
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
         }
     }
 }
